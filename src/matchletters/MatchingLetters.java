ccef2763bc1a49fa16fc42d4632a8503bd8ff6df
07elbekdev <masrapovelbek24@gmail.com>
2022-12-21 13:58:24 +0600

    something is changing...

--- src/matchletters/MatchingLetters.java
+++ src/matchletters/MatchingLetters.java
@@ -1,5 +1,19 @@
 package matchletters;
 
-public class MatchingLetters {
+import java.util.Scanner;
 
+public class MatchingLetters {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println(mathching(in.nextLine(), in.nextLine()));
+
+    }
+
+    public static boolean mathching(String a, String b) {
+
+        boolean result = a.matches(b);//here will compare the letters with each other.
+
+        return result;
+    }
 }
