2c8b7c8f0e61f856034b3d7bfb023fa7d416c9ac
07elbekdev <masrapovelbek24@gmail.com>
2022-12-21 14:27:58 +0600

    something is changing...

--- src/integerToRoman/IntegerToRoman.java
+++ src/integerToRoman/IntegerToRoman.java
@@ -1,8 +1,14 @@
 package integerToRoman;
 
+import java.util.Scanner;
+
 public class IntegerToRoman {
 
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+    }
+    public static String intToRoman(){
 
     }
 }
