056b69ea826085eb7d072de51355fc88263c2838
07elbekdev <masrapovelbek24@gmail.com>
2022-12-21 14:05:31 +0600

    something is changing...

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,32 @@ public class Main {
 
 
         Scanner in = new Scanner(System.in);
-        System.out.println(longestPalindrome(in.nextLine()));//this is with 20 th line until 25.
-
+        System.out.println(isPalindrome(in.nextInt()));
     }
 
-    public static String longestPalindrome(String s) {
-//    public static boolean isPalindrome(int x) {
-//        int r, sum = 0, temp;
-//        //It is the number variable to be checked for palindrome
-//
-//        temp = x;
-//        while (x > 0) {
-//            r = x % 10;  //getting remainder
-//            sum = (sum * 10) + r;
-//            x = x / 10;
-//        }
-//        if (temp == sum) {
-//            return true;
-//        } else {
-//            return false;
-//        }
-//    }
-//}
-        String res = "";
+    public static boolean isPalindrome(int x) {
+        int r, sum = 0, temp;
+        //It is the number variable to be checked for palindrome
+
+        temp = x;
+        while (x > 0) {
+            r = x % 10;  //getting remainder
+            sum = (sum * 10) + r;
+            x = x / 10;
+        }
+        if (temp == sum) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+//    public static String longestPalindrome(String s) {
+//        System.out.println(longestPalindrome(in.nextLine()));
+  /**      String res = "";
         for (int i = 0, j = 0, d = 0; j < s.length(); i += d, j += 1 - d, d = 1 - d)
             for (int l = i, r = j; l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r); l--, r++)
                 if (r + 1 - l > res.length()) res = s.substring(l, r + 1);
         return res;
     }
-}
+}*/
