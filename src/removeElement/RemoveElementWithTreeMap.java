9996183a28a3e89b45a3cc9cb27c1dd684a92c18
07elbekdev <masrapovelbek24@gmail.com>
2022-12-22 14:59:57 +0600

    I made it very good.

--- src/removeElement/RemoveElementWithTreeMap.java
+++ src/removeElement/RemoveElementWithTreeMap.java
@@ -1,13 +1,22 @@
 package removeElement;
 
+
+import java.util.ArrayList;
+
 public class RemoveElementWithTreeMap {
     public static void main(String[] args) {
-        int[]array = {1,2,3,4,5};
-        int value = 0;
+        int[]array = {2,35,27,9,35,47};
+        int value = 35;
         System.out.println(removeElement(array,value));
     }
             public static int removeElement(int[] nums, int val) {
+                ArrayList<Integer>arrayList = new ArrayList<>();
+                for (int i = 0; i < nums.length; i++) {
+                    if (nums[i]!=val){
+                        arrayList.add(nums[i]);
+                    }
+                }
 
-        return val;
+                return arrayList.size();
     }
 }
