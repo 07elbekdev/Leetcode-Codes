029158cc8a5d5e8f87dd4469243222d769404b01
07elbekdev <masrapovelbek24@gmail.com>
2022-12-22 14:17:16 +0600

    I started a new project...

--- src/removeElement/RemoveElementWithTreeMap.java
+++ src/removeElement/RemoveElementWithTreeMap.java
@@ -2,6 +2,12 @@ package removeElement;
 
 public class RemoveElementWithTreeMap {
     public static void main(String[] args) {
+        int[]array = {1,2,3,4,5};
+        int value = 0;
+        System.out.println(removeElement(array,value));
+    }
+            public static int removeElement(int[] nums, int val) {
 
+        return val;
     }
 }
