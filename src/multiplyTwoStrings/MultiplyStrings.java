737b3b115b59d0a2049ed257f494665eed7737f7
07elbekdev <masrapovelbek24@gmail.com>
2022-12-23 23:40:01 +0600

    Something is changing right now...

--- src/multiplyTwoStrings/MultiplyStrings.java
+++ src/multiplyTwoStrings/MultiplyStrings.java
@@ -4,26 +4,12 @@ import java.util.Scanner;
 
 public class MultiplyStrings {
     public static void main(String[] args) {
-            Scanner scn = new Scanner(System.in);
-            String str1 = scn.nextLine();
-            String str2 = scn.nextLine();
-        multiplication(str1,str2);
+        String a = "123456789";
+        String b = "987654321";
+        System.out.println(multiply(a,b));
     }
-//    public static void main(String[] args) {
+    public static String multiply(String num1, String num2) {
 
-    //        int num1 = 3;
-//        int num2 = 2;
-//        System.out.println(multiply(String.valueOf(num1),String.valueOf(num2)));
-//    }
-//        public static String multiply(String num1, String num2) {
-//        String str1 = String.valueOf(3);
-//        String str2 = String.valueOf(3);
-//        String result = multiply(str1,str2);
-//            return result;
-//        }
-//    }
-    public static String multiplication(String num1, String num2) {
-            String res = multiplication(num1, num2);
-            return res;
-        }
+        return String.valueOf(Integer.parseInt(num1)*Integer.parseInt(num2));
     }
+}
