204e391bfdb4cfc8c8fa9e40dfc87522d26ca166
07elbekdev <masrapovelbek24@gmail.com>
2022-12-23 16:30:51 +0600

    I think everything is ok

--- src/reverseString/ReverseString.java
+++ src/reverseString/ReverseString.java
@@ -2,11 +2,10 @@ package reverseString;
 
 public class ReverseString {
     public static void main(String[] args) {
-        String [] string = {"h","e","l","l","o"};
-        reverseString(string);
+        String s = "hello";
+        reverseString(s.toCharArray());
     }
-
-    public static void reverseString(String[] s) {
+    public static void reverseString(char[] s) {
         for (int i = s.length - 1; i >= 0; i--) {
             System.out.print(s[i]);
         }
