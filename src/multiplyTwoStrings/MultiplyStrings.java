481b602fd369467a5751caf986cbbb032f0b6972
07elbekdev <masrapovelbek24@gmail.com>
2022-12-24 00:55:58 +0600

    Finally I've finished this code myself

--- src/multiplyTwoStrings/MultiplyStrings.java
+++ src/multiplyTwoStrings/MultiplyStrings.java
@@ -2,14 +2,15 @@ package multiplyTwoStrings;
 
 public class MultiplyStrings {
     public static void main(String[] args) {
-        String a = "123456789";
-        String b = "987654321";
-        System.out.println(multiply(a,b));
+        String a = "6913259249";//here will multiply String by numbers even they are large.
+        String b = "7110334989";
+        System.out.println(multiply(a, b));
     }
+
     public static String multiply(String num1, String num2) {
         long i = Integer.parseInt(num1);
         long j = Integer.parseInt(num2);
-            String result = String.valueOf(i*j);
+        String result = (String.valueOf(i * j));
         return (String.valueOf(result));
     }
 }
