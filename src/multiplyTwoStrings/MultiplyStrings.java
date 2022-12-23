2113f16f6829e8ec768f8b01f7f5625e217b4973
07elbekdev <masrapovelbek24@gmail.com>
2022-12-23 22:35:29 +0600

    Something is changing right now...

--- src/multiplyTwoStrings/MultiplyStrings.java
+++ src/multiplyTwoStrings/MultiplyStrings.java
@@ -2,7 +2,9 @@ package multiplyTwoStrings;
 
 public class MultiplyStrings {
     public static void main(String[] args) {
-        System.out.println(multiply());
+        String num1 = "3";
+        String num2 = "2";
+        System.out.println(multiply(num1.intern(),num2.intern()));
     }
         public static String multiply(String num1, String num2) {
 
