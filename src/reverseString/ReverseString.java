5c7b8b93bc1d8b18b797fad9dc0e8425f79bfb6e
07elbekdev <masrapovelbek24@gmail.com>
2022-12-23 16:25:37 +0600

    I think everything is fresh.

--- src/reverseString/ReverseString.java
+++ src/reverseString/ReverseString.java
@@ -1,16 +1,14 @@
 package reverseString;
 
-import java.util.Arrays;
-
 public class ReverseString {
     public static void main(String[] args) {
-        char[]string = "h,e,l,l,o".toCharArray();
+        String [] string = {"h","e","l","l","o"};
         reverseString(string);
     }
-        public static void reverseString(char[] s) {
-            for (int i = s.length - 1; i >= 0; i--) {
-                System.out.println(s[i]);
-            }
-            System.out.println(Arrays.toString(s));
+
+    public static void reverseString(String[] s) {
+        for (int i = s.length - 1; i >= 0; i--) {
+            System.out.print(s[i]);
         }
     }
+}
