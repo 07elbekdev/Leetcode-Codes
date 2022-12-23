578ff38c44611fa26f8a3600002fdd33b3c9341b
07elbekdev <masrapovelbek24@gmail.com>
2022-12-24 02:09:47 +0600

    I'm also looking for something now...

--- src/plusOne/PlusOne.java
+++ src/plusOne/PlusOne.java
@@ -1,17 +1,19 @@
 package plusOne;
 
+import java.util.Arrays;
+
 public class PlusOne {
     public static void main(String[] args) {
         int[] number = {1, 9, 9, 9};
-        System.out.println(plusOne(number));
+        System.out.println(Arrays.toString(plusOne(number)));
     }
 
     public static int[] plusOne(int[] digits) {
+        for (int i = 0; i < ; i++) {
 
-        di
-        for (int as : digits) {
-            System.out.println(as);
+        Integer.sum(digits,1);
         }
-        return new int[]{0};
+        }
+        return null;
     }
 }
