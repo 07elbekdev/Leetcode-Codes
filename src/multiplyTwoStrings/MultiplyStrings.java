c6365b7ce31a177c073f4f76c6cb9c3782388721
07elbekdev <masrapovelbek24@gmail.com>
2022-12-23 22:31:10 +0600

    Something is changing right now...

--- src/multiplyTwoStrings/MultiplyStrings.java
+++ src/multiplyTwoStrings/MultiplyStrings.java
@@ -2,6 +2,9 @@ package multiplyTwoStrings;
 
 public class MultiplyStrings {
     public static void main(String[] args) {
-
+        System.out.println(multiply());
     }
-}
+        public static String multiply(String num1, String num2) {
+
+        }
+    }
