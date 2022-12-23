7c1e18b1a833b0eabcd24ba1088dd9ab42b5b564
07elbekdev <masrapovelbek24@gmail.com>
2022-12-24 01:46:22 +0600

    I'm also looking for something now.

---
+++ src/plusOne/PlusOne.java
@@ -0,0 +1,12 @@
+package plusOne;
+
+public class PlusOne {
+    public static void main(String[] args) {
+        int[]number = {1,2,3,4};
+        System.out.println(plusOne(number));
+    }
+        public static int[] plusOne(int[] digits) {
+            int result =
+            return result;
+    }
+}
