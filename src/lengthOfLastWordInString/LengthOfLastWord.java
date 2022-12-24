9df235c8c8ef63c9cc0b25815ef142680bbf8f6f
07elbekdev <masrapovelbek24@gmail.com>
2022-12-24 18:54:05 +0600

    I've finished everything.

--- src/lengthOfLastWordInString/LengthOfLastWord.java
+++ src/lengthOfLastWordInString/LengthOfLastWord.java
@@ -2,6 +2,10 @@ package lengthOfLastWordInString;
 
 public class LengthOfLastWord {
     public static void main(String[] args) {
-
+        String word = "Hello World";
+        System.out.println(lengthOfLastWord(word));
     }
-}
+        public static int lengthOfLastWord(String s) {
+
+        }
+    }
