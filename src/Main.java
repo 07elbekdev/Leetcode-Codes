19b604cd58102fdde70d08cb7fb73dfca6bfb16b
07elbekdev <masrapovelbek24@gmail.com>
2022-12-24 10:48:57 +0600

    I've finished everything.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner in = new Scanner(System.in);
         System.out.println(isPalindrome(in.nextInt()));
     }
