d436e8a9e19c269cd3b4c6ee0103d0793fb26d50
07elbekdev <masrapovelbek24@gmail.com>
2022-12-24 09:22:07 +0600

    Changing something...

--- src/multiplyTwoStrings/MultiplyStrings.java
+++ src/multiplyTwoStrings/MultiplyStrings.java
@@ -4,20 +4,17 @@ import java.math.BigInteger;
 
 public class MultiplyStrings {
     public static void main(String[] args) {
-        String a = "6913259249";//here will multiply two Strings by number, even they are large.
-        String b = "7110334989";
+        String a = "69";//here will multiply two Strings by number, even they are large.
+        String b = "719";
         System.out.println(multiply(a, b));
     }
 
     public static String multiply(String num1, String num2) {
 
-          long i = Long.parseLong(String.valueOf(num1));
-          long j = Long.parseLong(String.valueOf(num2));
-          String result = String.valueOf(i+j);
-//        BigInteger number1 = new BigInteger(num1);
-//        BigInteger number2 = new BigInteger(num2);
-//        BigInteger multiply = number1.multiply(number2);
-//
-//        return (multiply.toString());
+        BigInteger number1 = new BigInteger(num1);
+        BigInteger number2 = new BigInteger(num2);
+        BigInteger multiply = number1.add(number2);
+
+        return (multiply.toString());
     }
 }
