adf1a3ee22b8e08566dbd15e208c2d14aad07e8c
07elbekdev <masrapovelbek24@gmail.com>
2022-12-24 10:55:31 +0600

    I've finished everything.

--- src/superPow/SuperPow.java
+++ src/superPow/SuperPow.java
@@ -1,5 +1,17 @@
 package superPow;
 
 public class SuperPow {
+    public static void main(String[] args) {
+        int a = 3;
+        int[]pow = {1,0};
+        System.out.println(superPow(a,pow));
+    }
+        public static int superPow(int a, int[] b) {
+            for (int num : b) {
+                return num;
+            }
+            int result = (int) Math.pow(a,num);
 
-}
+            return result;
+        }
+    }
