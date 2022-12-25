f5f1e9813caf38d07f9940d865ddfbc527829793
07elbekdev <masrapovelbek24@gmail.com>
2022-12-25 19:14:51 +0600

    I've finished everything.

--- src/plusOne/PlusOne.java
+++ src/plusOne/PlusOne.java
@@ -1,19 +1,27 @@
 package plusOne;
 
+import java.util.Arrays;
+
 public class PlusOne {
 
     public static void main(String[] args) {
-        int[]arr = {1,9,9,9};
-        System.out.println(plusOne(arr));
+        int[] arr = {3,6,5,4,3};
+        System.out.println(Arrays.toString(plusOne(arr)));
     }
-
-    public static int[] plusOne(int[] digits) {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int num : digits) {
-            stringBuilder.append(num);
+        public static int[] plusOne(int[] digits){
+            for (int i = digits.length - 1; i >= 0; i--) {
+                if (digits[i] != 9) {
+                    digits[i]++;
+                    break;
+                } else {
+                    digits[i] = 0;
+                }
+            }
+            if (digits[0] == 0) {
+                int[] rst = new int[digits.length + 1];
+                rst[0] = 1;
+                return rst;
+            }
+            return digits;
         }
-        int transform = Integer.parseInt(stringBuilder.toString());
-        int result = transform+1;
-        return new int[result];
     }
-}
