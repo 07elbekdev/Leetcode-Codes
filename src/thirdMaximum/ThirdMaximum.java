85c282da031561269971963bbfe5421722db88ac
07elbekdev <masrapovelbek24@gmail.com>
2022-12-28 19:59:45 +0600

    I've finished everything.

--- src/thirdMaximum/ThirdMaximum.java
+++ src/thirdMaximum/ThirdMaximum.java
@@ -8,19 +8,20 @@ public class ThirdMaximum {
         int[] maximum = {2, 2, 3, 1};
         System.out.println(thirdMax(maximum));
     }
-            public static int thirdMax(int[] nums) {
-                Arrays.sort(nums,0,nums.length);
 
-                int count=0;
-                int max=nums[nums.length-1];
-                for(int i=nums.length-2;i>=0;i--)   {
-                    if(count==2)    break;
-                    if(nums[i]<max) {
-                        count++;
-                        max=nums[i];
-                    }
-                }
-                if(count==2)    return max;
-                return nums[nums.length-1];
+    public static int thirdMax(int[] nums) {
+        Arrays.sort(nums, 0, nums.length);
+
+        int count = 0;
+        int max = nums[nums.length - 1];
+        for (int i = nums.length - 2; i >= 0; i--) {
+            if (count == 2) break;
+            if (nums[i] < max) {
+                count++;
+                max = nums[i];
             }
         }
+        if (count == 2) return max;
+        return nums[nums.length - 1];
+    }
+}
