b92ba913f085cfcbb79c4764197197e0fd042dbb
07elbekdev <masrapovelbek24@gmail.com>
2023-01-01 20:59:23 +0600

    I've finished everything.

--- src/validNumber/ValidNumber.java
+++ src/validNumber/ValidNumber.java
@@ -16,3 +16,15 @@ public class ValidNumber {
         return false;
     }
 }
+//import java.math.BigDecimal;//todo: this is the shortest way of that solution using BigDecimal.
+//class Solution {
+//    public boolean isNumber(String s) {
+//        try{
+//            new BigDecimal(s);
+//        }catch(Exception e){
+//            String msg = e.getMessage();
+//            return "Too many nonzero exponent digits.".equals(msg) || "Exponent overflow.".equals(msg);
+//        }
+//        return true;
+//    }
+//}
