11c0bde0b64b7217a2d864644ae3714077c17c50
07elbekdev <masrapovelbek24@gmail.com>
2023-01-01 20:57:02 +0600

    I've finished everything.

---
+++ src/validNumber/ValidNumber.java
@@ -0,0 +1,18 @@
+package validNumber;
+
+public class ValidNumber {
+    public static void main(String[] args) {
+        String a = ".1";
+        System.out.println(isNumber(a));
+    }
+
+    public static boolean isNumber(String s) {
+        String[] array = {".1","0","2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7", "+6e-1", "53.5e93", "-123.456e789"};
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].equals(s)){
+                return true;
+            }
+        }
+        return false;
+    }
+}
