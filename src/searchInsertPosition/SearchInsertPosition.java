b8f39dce9404e74afa3edf270f7e42531444caf1
07elbekdev <masrapovelbek24@gmail.com>
2023-01-02 21:48:50 +0600

    Everything is up to-date.

--- src/searchInsertPosition/SearchInsertPosition.java
+++ src/searchInsertPosition/SearchInsertPosition.java
@@ -1,8 +1,5 @@
 package searchInsertPosition;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class SearchInsertPosition {
     public static void main(String[] args) {
         int[] array = {1, 3, 5, 6};
@@ -12,7 +9,8 @@ public class SearchInsertPosition {
 
     public static int searchInsert(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == target || target<nums[i]) { return i;
+            if (nums[i] == target || target < nums[i]) {
+                return i;
             }
         }
         return nums.length;
