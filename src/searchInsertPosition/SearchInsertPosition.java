6ce34664402b3cfd07743a9293511d79b00f6425
07elbekdev <masrapovelbek24@gmail.com>
2023-01-02 21:23:59 +0600

    I added also something.

---
+++ src/searchInsertPosition/SearchInsertPosition.java
@@ -0,0 +1,13 @@
+package searchInsertPosition;
+
+public class SearchInsertPosition {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        int target = 6;
+        System.out.println(searchInsert(array, target));
+    }
+
+    public static int searchInsert(int[] nums, int target) {
+
+    }
+}
