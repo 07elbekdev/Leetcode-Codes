00808c3f898161fd4c158216c2b114e34c745746
07elbekdev <masrapovelbek24@gmail.com>
2023-01-09 21:08:34 +0600

    Everything is up to-date.

--- src/countOddNumbersInAnInteger/CountOddNumbersInAnIntervalRange.java
+++ src/countOddNumbersInAnInteger/CountOddNumbersInAnIntervalRange.java
@@ -8,7 +8,7 @@ public class CountOddNumbersInAnIntervalRange {
     }
 
     public static int countOdds(int low, int high) {
-        if (low%2==0 & high%2==0)return (high-low)/2;
-        return ((high-low)/2)+1;
+        if (low % 2 == 0 & high % 2 == 0) return (high - low) / 2;
+        return ((high - low) / 2) + 1;
     }
 }
