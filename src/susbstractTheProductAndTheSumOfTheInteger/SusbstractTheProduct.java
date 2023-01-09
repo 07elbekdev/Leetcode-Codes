97700c26f991bc7d9c2b82fe5b90c3aef8ddd7ec
07elbekdev <masrapovelbek24@gmail.com>
2023-01-09 22:32:17 +0600

    Everything is up to-date.

---
+++ src/susbstractTheProductAndTheSumOfTheInteger/SusbstractTheProduct.java
@@ -0,0 +1,13 @@
+package susbstractTheProductAndTheSumOfTheInteger;
+
+public class SusbstractTheProduct {
+    public static void main(String[] args) {
+        int number = 234;
+        System.out.println(subtractProductAndSum(number));
+    }
+
+    public static int subtractProductAndSum(int n) {
+
+        return n;
+    }
+}
